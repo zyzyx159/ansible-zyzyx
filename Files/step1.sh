640207d242c343bcce6e53db5aed894f6ad743ab
Daniel Stellmon <dstellm@gmail.com>
2022-06-25 00:24:05 -0500

    added autoremove

--- Files/step1.sh
+++ Files/step1.sh
@@ -5,6 +5,7 @@ set -e
 patching(){
     apt update -y
     DEBIAN_FRONTEND=NONINTERACTIVE apt upgrade -y
+    DEBIAN_FRONTEND=NONINTERACTIVE apt autoremove -y
     DEBIAN_FRONTEND=NONINTERACTIVE apt install ansible -y
 };
 
