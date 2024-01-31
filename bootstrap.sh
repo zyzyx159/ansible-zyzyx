866bc6f69eebec627ccfdd688f464cba54782c08
Daniel Stellmon <Dstellm@gmail.com>
2024-01-31 01:18:08 -0600

    added notes

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,14 @@
 # usermod -a -G sudo zyzyx
 # exit, log out, log back in
 
+# minimal base line apps for running ansible
 sudo apt -qq update && sudo apt install git, python3, wget, gpg
+
+# install ansible
 UBUNTU_CODENAME=jammy #use the ubuntu code name
 wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt update && sudo apt install ansible
 
+# run ansible
 ansible-pull -U https://github.com/zyzyx159/bootstrap.git -i "$(hostname --short)," --ask-become-pass
