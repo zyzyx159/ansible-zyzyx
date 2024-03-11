5fd702b60a1ce7851760d6ee8fff958394544599
zyzyx159 <40427026+zyzyx159@users.noreply.github.com>
2024-03-11 05:12:14 -0500

    added nala and starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # minimal base line apps for running ansible
-apt -qq update && apt -y install git python3 wget gpg
+apt -qq update && apt -y install git python3 wget gpg nala
 
-# install ansible
+# install ansible repository
 UBUNTU_CODENAME=jammy #use the ubuntu code name
 wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/ansible.list
-apt update && apt install ansible
+
+# Run nala updateer
+nala fetch --fetches 16 --assume-yes --auto
+
+# install ansible
+nala install ansible
+
+# install starship - its not in the offical repos
+curl -O https://starship.rs/install.sh | sh
 
 # run ansible
 ansible-pull -U https://github.com/zyzyx159/bootstrap.git -i "$(hostname --short),"
