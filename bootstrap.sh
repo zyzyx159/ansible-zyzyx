5a295113776de10e48beb89f501bc574b0c09f00
zyzyx159 <40427026+zyzyx159@users.noreply.github.com>
2024-03-11 03:04:23 -0500

    remove dupe git install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # minimal base line apps for running ansible
-apt -qq update && apt install git, python3, wget, gpg, git
+apt -qq update && apt install git, python3, wget, gpg
 
 # install ansible
 UBUNTU_CODENAME=jammy #use the ubuntu code name
