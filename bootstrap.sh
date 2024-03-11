56e85d7bd3f81a1d06e9925b18a1ce9fe1ca455a
zyzyx159 <40427026+zyzyx159@users.noreply.github.com>
2024-03-11 03:04:55 -0500

    removed another sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ apt -qq update && apt install git, python3, wget, gpg
 UBUNTU_CODENAME=jammy #use the ubuntu code name
 wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/ansible.list
-apt update && sudo apt install ansible
+apt update && apt install ansible
 
 # run ansible
 ansible-pull -U https://github.com/zyzyx159/bootstrap.git -i "$(hostname --short),"
